87057d8c2a782f59a5d772035bf4a6798ed1501e
maryker <mashalipatowa@yandex.ru>
2023-04-04 20:14:23 +0500

    step 3

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/maryker/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/maryker/python-project-50/actions)
+
+[![asciicast](https://asciinema.org/a/Iwow6zkS7oF7M9X8VAq63mRPq.svg)](https://asciinema.org/a/Iwow6zkS7oF7M9X8VAq63mRPq)
--- hexlet_code/scripts/gendiff.py
+++ hexlet_code/scripts/gendiff.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 
 
@@ -9,10 +10,35 @@ parser = argparse.ArgumentParser(
 parser.add_argument('first_file')
 parser.add_argument('second_file')
 parser.add_argument('-f', '--format', help='set format of output')
-parser.parse_args()
+args = parser.parse_args()
+
+
+def generate_diff(file1, file2):
+    f1 = json.load(open(file1))
+    f2 = json.load(open(file2))
+    result = {}
+    keys = set(f1).union(set(f2))
+    for key in sorted(keys):
+        if key in f1:
+            if f1.get(key, None) == f2.get(key, None):
+                result[f'   {key}'] = f1[key]
+            else:
+                result[f' - {key}'] = f1[key]
+                if key in f2:
+                    result[f' + {key}'] = f2[key]
+        else:
+            result[f' + {key}'] = f2[key]
+    return '{\n' + '\n'.join([f'{key}: {bool_to_str(value)}' for key, value in result.items()]) + '\n}'
+
+
+def bool_to_str(value):
+    if isinstance(value, bool):
+        return str(value).lower()
+    return value
+
 
 def main():
-    pass
+    print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
