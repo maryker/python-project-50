589350bb77a1d310d3e720d5e7f9c47a5f9865cd
maryker <mashalipatowa@yandex.ru>
2023-07-10 22:36:50 +0500

    fixes

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,24 +2,15 @@ import pytest
 from gendiff import generate_diff
 
 
-f1 = open('tests/fixtures/fixture_plain.txt')
-f2 = open('tests/fixtures/fixture_deep.txt')
-
-res_plain = f1.read()
-res_deep = f2.read()
-
-f1.close()
-f2.close()
-
-
-@pytest.mark.parametrize("file1,file2,res",
+@pytest.mark.parametrize("file1,file2,result",
                          [('tests/fixtures/file1.json',
-                           'tests/fixtures/file2.json', res_plain),
+                           'tests/fixtures/file2.json', 'tests/fixtures/fixture_plain.txt'),
                           ('tests/fixtures/file1.yml',
-                           'tests/fixtures/file2.yml', res_plain),
+                           'tests/fixtures/file2.yml', 'tests/fixtures/fixture_plain.txt'),
                           ('tests/fixtures/file3.json',
-                           'tests/fixtures/file4.json', res_deep),
+                           'tests/fixtures/file4.json', 'tests/fixtures/fixture_deep.txt'),
                           ('tests/fixtures/file3.yml',
-                           'tests/fixtures/file4.yml', res_deep)])
-def test_deep(file1, file2, res):
-    assert generate_diff(file1, file2) == res
+                           'tests/fixtures/file4.yml', 'tests/fixtures/fixture_deep.txt')])
+def test_deep(file1, file2, result):
+    with open(result, 'r') as res:
+        assert generate_diff(file1, file2) == res.read()
